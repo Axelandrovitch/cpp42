f041c76e8e703d8d48f684981110b362ba6765c4
Axelandrovitch <axel.a.hetru@gmail.com>
2025-09-15 13:22:36 +0200

    completed logic of ex04: OK

--- mod01/ex04/main.cpp
+++ mod01/ex04/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <string>
+
+
 
 int ftSed(char **argv) {
 
   std::ifstream infile(argv[1]);
-
   if (!infile) {
     std::cerr << "Error: " << std::strerror(errno) << std::endl;
     return errno;
@@ -13,13 +15,27 @@ int ftSed(char **argv) {
 
   std::string outFileName = std::string(argv[1]) + ".replace";
   std::ofstream outfile(outFileName.c_str());
-
   if (!outfile) {
     std::cerr << "Error: " << std::strerror(errno) << std::endl;
     return errno;
   }
-  outfile << "Hello World!\nFrom outfile.txt\n";
   
+  std::string oldStr = argv[2];
+  std::string newStr = argv[3];
+  std::string currentLine;
+
+  while(std::getline(infile, currentLine)) {
+    size_t pos = 0;
+    while((pos = currentLine.find(oldStr, pos)) != std::string::npos) {
+      currentLine.erase(pos, oldStr.length());
+      currentLine.insert(pos, newStr);
+      pos += newStr.length();
+    }
+    outfile << currentLine << std::endl;
+  }
+
+  infile.close();
+  outfile.close();
   return 0;
 }
 
@@ -29,6 +45,9 @@ int main(int argc, char **argv) {
     std::cout << "Usage:" << argv[0] << " <filename>" << " <string to replace>" << " <string to replace with>\n";
     return 0;
   }
-
+  if (argv[2][0] == '\0') {
+    std::cerr << "Error: s1 cannot be an empty string." << std::endl;
+    return 1;
+  }
   return ftSed(argv);
 }
