a99250246e93d3bffb36f60226d1f5d1bc004edd
Axelandrovitch <axel.a.hetru@gmail.com>
2025-09-15 14:04:35 +0200

    add ex00

---
+++ mod01/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+int main() {
+    Zombie* heapZombie = newZombie("Heapster");
+    heapZombie->announce();
+    delete heapZombie;
+
+    randomChump("Stacky");
+
+    return 0;
+}
+
