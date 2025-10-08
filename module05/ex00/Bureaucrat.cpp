e70b96801f5c4d6ffe79259b0ecb5ba09baeaaf7
Axel Hetru <ahetru@cbr6s1.42barcelona.com>
2025-10-08 11:32:34 +0200

    add base of ex00 module 05

---
+++ module05/ex00/Bureaucrat.cpp
@@ -0,0 +1,7 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {};
+Bureaucrat::Bureaucrat(const std::string name, unsigned int grade) {
+	_name = name;
+	_grade = grade;
+};
---
+++ module05/ex00/BureaucratException.hpp
@@ -0,0 +1,10 @@
+#ifndef BUREAUCRATEXCEPTION_HPP
+#define BUREAUCRATEXCEPTION_HPP
+
+#include <bits/stdc++.h>
+
+class BureaucratException : public std::exception {
+	
+};
+
+#endif
