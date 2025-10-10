d94a48d33f98de4aa0b75979212eea6d00c9cfc0
Axelandrovitch <axel.a.hetru@gmail.com>
2025-10-10 14:57:21 +0200

    add module05/ex00 with tests: OK

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -1,5 +1,50 @@
 #include "Bureaucrat.hpp"
 
 int main(void) {
-	return 0;
+    try {
+        Bureaucrat bobby("Bobby", 151);
+        std::cout << bobby << std::endl;
+    }
+    catch (const Bureaucrat::GradeTooLowException& e) {
+        std::cerr << "Error : " << e.what() << std::endl;
+    }
+
+    try {
+        Bureaucrat bobbynator("bobbynator", 0);
+        std::cout << bobbynator << std::endl;
+    }
+    catch (const Bureaucrat::GradeTooHighException& e) {
+        std::cerr << "Error" << e.what() << std::endl;
+    }
+
+    try {
+        Bureaucrat bobbynette("bobbynette", 2);
+        std::cout << bobbynette << std::endl;
+
+        bobbynette.incrementGrade();
+        std::cout << "After increment: " << bobbynette << std::endl;
+
+        bobbynette.incrementGrade();
+        std::cout << "After increment: " << bobbynette << std::endl;
+    }
+    catch (const Bureaucrat::GradeTooHighException& e) {
+        std::cerr << "Error : " << e.what() << std::endl;
+    }
+
+    try {
+        Bureaucrat dave("Dave", 149);
+        std::cout << dave << std::endl;
+
+        dave.decrementGrade();
+        std::cout << "After decrement: " << dave << std::endl;
+
+        dave.decrementGrade();
+        std::cout << "After decrement: " << dave << std::endl;
+    }
+    catch (const Bureaucrat::GradeTooLowException& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
+
